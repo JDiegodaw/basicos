663d7b272d86aa4face8c8036c2f850c01725f25
P1nXo <P1nXo@192.168.56.1>
2018-10-29 21:30:30 +0100

    cadenas

--- src/basicosjuandiegogomez/BasicosJuanDiegoGomez.java
+++ src/basicosjuandiegogomez/BasicosJuanDiegoGomez.java
@@ -8,5 +8,14 @@ public class BasicosJuanDiegoGomez {
     public static void main(String[] args) {
        
     }
-    
+    public static void cadenas (){
+        
+        String cadena = new String ();
+        cadena = "cadenazo";
+       
+        System.err.println("Valor cadena");
+        System.out.println(cadena);
+        
+    }
+
 }
