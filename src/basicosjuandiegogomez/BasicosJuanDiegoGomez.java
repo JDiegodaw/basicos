75495b728ac15eec8428a07bf49f5f60a190c87f
P1nXo <P1nXo@192.168.56.1>
2018-10-29 21:39:42 +0100

    logicos

--- src/basicosjuandiegogomez/BasicosJuanDiegoGomez.java
+++ src/basicosjuandiegogomez/BasicosJuanDiegoGomez.java
@@ -6,7 +6,20 @@ public class BasicosJuanDiegoGomez {
 
    
     public static void main(String[] args) {
-       
+    logicos();   
     }
-    
+    public static void logicos(){
+          boolean x = true;
+          boolean y = false;
+          boolean a1 = x && x;
+          boolean a2 = x && y;
+          boolean a3 = y && x;
+          boolean a4 = y && y;
+
+          
+          System.out.println( x + " AND " + x + " = " + a1 );
+          System.out.println( x + " AND " + y + " = " + a2 );
+          System.out.println( y + " AND " + x + " = " + a3 );
+          System.out.println( y + " AND " + y + " = " + a4 );
+    }
 }
