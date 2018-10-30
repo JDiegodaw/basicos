af0e95fdd2126e758b7290dd5230cb089eabe1be
P1nXo <P1nXo@192.168.56.1>
2018-10-30 01:57:22 +0100

    Merge

--- src/basicosjuandiegogomez/BasicosJuanDiegoGomez.java
+++ src/basicosjuandiegogomez/BasicosJuanDiegoGomez.java
@@ -1,4 +1,3 @@
-
 package basicosjuandiegogomez;
 
 
@@ -6,16 +5,69 @@ public class BasicosJuanDiegoGomez {
 
    
     public static void main(String[] args) {
+
+    logicos();   
     cadenas();   
+    numericos();
     }
-    public static void cadenas (){
+    
+    public static void logicos(){
+          boolean x = true;
+          boolean y = false;
+          boolean a1 = x && x;
+          boolean a2 = x && y;
+          boolean a3 = y && x;
+          boolean a4 = y && y;
+
+          
+          System.out.println( x + " AND " + x + " = " + a1 );
+          System.out.println( x + " AND " + y + " = " + a2 );
+          System.out.println( y + " AND " + x + " = " + a3 );
+          System.out.println( y + " AND " + y + " = " + a4 );
+          
+          a1 = x || x;
+          a2 = x || y;
+          a3 = y || x;
+          a4 = y || y;
+          
+          System.out.println( x + " OR " + x + " = " + a1 );
+          System.out.println( x + " OR " + y + " = " + a2 );
+          System.out.println( y + " OR " + x + " = " + a3 );
+          System.out.println( y + " OR " + y + " = " + a4 );
+    }
+    
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+
+    
+   
         
+    }
+
+    private static void cadenas() {
         String cadena = new String ();
         cadena = "cadenazo";
        
         System.err.println("Valor cadena");
         System.out.println(cadena);
-        
     }
+
 
 }
